5bfc2d08687a08a13e09c075572da72e6a682887
David Lewis <dil20@sussex.ac.uk>
2018-04-16 21:40:56 +0100

    Initialize project, Create boid and flock manager Script outlines.

---
+++ Flocking/Assets/Scripts/FlockManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //take 3d models, replicate them multiple times
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*       
+        loop through models call relevant method (wander,seek etc) depending on models current state.
+        or.. each model has its own update method and calls its own action method?
+                
+        */
+
+    }
+
+
+
+
+
+}
